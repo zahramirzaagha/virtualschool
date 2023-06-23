7a51911974f224b28a209b1b0daf1d45c168007e
Neda Mirzaagha <nedamirzaagha@SASANs-Mac-mini.local>
2023-06-23 18:53:47 -0400

    created course entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -45,11 +45,15 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, \Serial
     #[ORM\Column(type: 'boolean')]
     private ?bool $isVerified;
 
+    #[ORM\OneToMany(mappedBy: 'instructor', targetEntity: Course::class, orphanRemoval: true)]
+    private Collection $courses;
+
     public function __construct()
     {
         $this->isActive = true;
         $this->isVerified = true;
         $this->roles = new ArrayCollection();
+        $this->courses = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -172,6 +176,33 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, \Serial
     public function setIsVerified(bool $isVerified): static
     {
         $this->isVerified = $isVerified;
+
+        return $this;
+    }
+
+    public function getCourses(): Collection
+    {
+        return $this->courses;
+    }
+
+    public function addCourse(Course $course): static
+    {
+        if (!$this->courses->contains($course)) {
+            $this->courses->add($course);
+            $course->setInstructor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCourse(Course $course): static
+    {
+        if ($this->courses->removeElement($course)) {
+            // set the owning side to null (unless already changed)
+            if ($course->getInstructor() === $this) {
+                $course->setInstructor(null);
+            }
+        }
 
         return $this;
     }
